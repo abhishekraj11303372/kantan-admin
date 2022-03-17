4d6429f258b829ebcca6b25d341d95c4c8918a6f
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-17 19:23:49 +0530

    pie chart

---
+++ src/components/charts/Charts.jsx
@@ -0,0 +1,9 @@
+import './charts.scss'
+
+function Charts() {
+  return (
+    <div className='chart'>Charts</div>
+  )
+}
+
+export default Charts
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,8 @@ import "./home.scss"
 import Sidebar from "../../components/sidebar/Sidebar.jsx"
 import Navbar from "../../components/navbar/Navbar.jsx"
 import Widget from "../../components/widget/Widget"
+import Featured from "../../components/featured/Featured"
+import Charts from "../../components/charts/Charts"
 
 function Home() {
   return (
@@ -14,6 +16,10 @@ function Home() {
           <Widget type="order"/>
           <Widget type="earning"/>
           <Widget type="balance"/>
+        </div>
+        <div className="charts">
+          <Featured/>
+          <Charts />
         </div>
       </div>
     </div>
