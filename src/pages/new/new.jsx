835eb05ca4f36ba585b45fe49da0260628f864a3
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-19 00:04:53 +0530

    new init

--- src/pages/new/new.jsx
+++ src/pages/new/new.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import "./new.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Charts from "../../components/charts/Charts"
+import TableList from "../../components/tablelist/TableList.jsx"
 
 function New() {
   return (
-    <div>new</div>
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1 className="lastTransactions">Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              {/* <img className="itemImg" src="https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" /> */}
+              <div className="details">
+                <h1 className="itemTitle">Marsh Mallow</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>  
+                  <span className="itemValue">abc@marwshmallow.com</span>  
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>  
+                  <span className="itemValue">1234567890</span>  
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Adress:</span>  
+                  <span className="itemValue">Tokyo tower, beside CCD, H-15</span>  
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>  
+                  <span className="itemValue">Japan</span>  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            {/* <Charts aspect={150} title="User spending (last 7 months)"/> */}
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
