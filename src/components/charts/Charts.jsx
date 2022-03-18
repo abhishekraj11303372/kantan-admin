5dc89b35d8c717f93c8a59955937ce3ab3f8d7fe
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-18 09:20:21 +0530

    chart design library added

--- src/components/charts/Charts.jsx
+++ src/components/charts/Charts.jsx
@@ -1,8 +1,77 @@
 import './charts.scss'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    name: 'January',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'February',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'March',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'April',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'May',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'June',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'July',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 function Charts() {
   return (
-    <div className='chart'>Charts</div>
+    <div className='chart'>
+      <div className="title">Last 7 Months Revenue</div>
+      <ResponsiveContainer width="100%" height="100%" >
+      <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" className='gridChart'/>
+          <XAxis dataKey="name" stroke="gray"/>
+          {/* <YAxis /> */}
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+          {/* <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" /> */}
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
