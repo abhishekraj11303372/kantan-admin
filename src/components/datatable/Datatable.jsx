1f8581fdb7e17db8a88c88f637e6780966b6ac4d
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-18 21:41:11 +0530

    single init

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -3,13 +3,29 @@ import { DataGrid } from '@mui/x-data-grid'
 import {userColumns,rows} from '../../datatable.js'
 
 function Datatable() {
+
+const actionColumn = [{
+    field: "action",
+    headerName: "Actoin",
+    width: 200,
+    renderCell: () => {
+      return (
+        <div className="cellAction">
+          <div className="viewButton">View</div>
+          <div className="deleteButton">Delete</div>
+        </div>
+      );
+    },
+  },
+];
+  
   return (
     <div className='datatable'>
         <DataGrid
           rows={rows}
-          columns={userColumns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
+          columns={userColumns.concat(actionColumn)}
+          pageSize={7}
+          rowsPerPageOptions={[7]}
           checkboxSelection
         />
   </div>
--- src/pages/single/single.jsx
+++ src/pages/single/single.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import "./single.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
 
 function Single() {
   return (
-    <div>S</div>
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <h1 className="title">Left</h1>
+            <div className="item">
+              <img className="itemImg" src="https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+              details
+            </div>
+          </div>
+          <div className="right">
+            <h1 className="title">Right</h1>
+          </div>
+        </div>
+        <div className="bottom">
+          bottom
+        </div>
+      </div>
+    </div>
   )
 }
 
